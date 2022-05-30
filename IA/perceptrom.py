295d3ea2b0d91f6403de57f6d300d85077dae786
WFS <walkerf082@gmail.com>
2022-05-29 22:22:56 -0300

    .

--- IA/perceptrom.py
+++ IA/perceptrom.py
@@ -1,54 +1,95 @@
+import pandas 
 #implementando o algoritmo perceptrom. 
 #weights: quanto impacto cada dado deve ter no processo de aprendizagem.
 #row: linhas da tabela de treinamento ou teste.
 #l_rate: learing rate, usado para limitar o quanto cada nodo é corrigido toda vez que ele [nodo] é atualizado
 #epoch: numero de vezes que deve "estudar" a base de teste enquanto atualiza o peso
-
 #
+
 def predict(row, weights):
-    print("--------------------------------------------------------------------------------------------------------------------")
     activation = weights[0]
-    print(len(row)-1)
     for i in range(len(row)-1):
-        print("=====================")
-        print("i = ",i)
-        print("row[i] = ", row[i])
-        print("=====================")
         activation += weights[i + 1] * row[i]
     return 1.0 if activation >= 0.0 else 0.0
 
 
 #treinando os pesos dos inputs 
-def train_weights(tab_train, l_rate, n_epoch)
+def train_weights(tab_train, l_rate, n_epoch):
     weights = [0.0 for i in range(len(tab_train[0]))] #criando uma lista vazia com a mesma quantidade de colunas de tab_train
     for epoch in range(n_epoch):
         sum_error = 0.0
         for row in tab_train:
             prediction = predict(row, weights)             
-                       #indo para a ultima posicao do array
-            error = row[-1] - prediction
-            sum_error += error**2
+            error = row[-1] - prediction #indo para a ultima posicao do array
+            sum_error += error**2 
             weights[0] = weights[0] + l_rate * error
             for i in range(len(row)-1):
                 weights[i + 1] = weights[i + 1] + l_rate * error * row[i]
-        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
     return weights   
 
+#formata o output
+def perceptron(train, test, l_rate, n_epoch):
+    predictions = list()
+    weights = train_weights(train, l_rate, n_epoch)
+    print('==========Tabela de respostas==========')
+    for row in test:
+        prediction = predict(row, weights)
+        print('entrada01=%d, entrada02=%d, resposta_verdadeira=%d, agoritmo_resposta=%d' % (row[1], row[2], row[-1], prediction))
+        predictions.append(prediction)
+    print('=======================================')
+    return(predictions)
 
+#aplicando o perceptron para AND
+datasetTreinoAND = [[0, 0, 0],  
+                    [1, 0, 0],  
+                    [0, 1, 0],  
+                    [1, 1, 1]] 
 
-# testes          W1          W2
-dataset =   [[2.7810836,2.550537003,0],
-            [1.465489372,2.362125076,0],
-            [3.396561688,4.400293529,0],
-            [1.38807019,1.850220317,0],
-            [3.06407232,3.005305973,0],
-            [7.627531214,2.759262235,1],
-            [5.332441248,2.088626775,1],
-            [6.922596716,1.77106367,1],
-            [8.675418651,-0.242068655,1],
-            [7.673756466,3.508563011,1]]
-          #bias         X1                     X2 
-weights = [-0.1, 0.20653640140000007, -0.23418117710000003]
-for row in dataset:
-    prediction = predict(row, weights)
-    print("Expected=%d, Predicted=%d" % (row[-1], prediction))
+datasetAND  = [[0, 1, 0],  
+               [1, 1, 1],  
+               [1, 0, 0],  
+               [0, 0, 0],  
+               [1, 1, 1],  
+               [0, 1, 0],  
+               [1, 0, 0],  
+               [0, 1, 0],  
+               [0, 1, 0],  
+               [1, 1, 1]]
+
+datasetTreinoOR = [[0, 0, 0],  
+                   [1, 0, 1],  
+                   [0, 1, 1],  
+                   [1, 1, 1]]  
+
+datasetOr = [[0, 1, 1],  
+             [0, 0, 0],  
+             [1, 0, 1],  
+             [0, 0, 0],  
+             [1, 1, 1],  
+             [0, 1, 1],  
+             [1, 0, 1],  
+             [0, 1, 1],  
+             [0, 1, 1],  
+             [0, 0, 0]]
+
+# XOR
+datasetTreinoXOR = [[0, 0, 0],  
+                    [1, 0, 1],  
+                    [0, 1, 1],  
+                    [1, 1, 0]]
+
+datasetXor = [[0, 1, 1],  
+              [0, 0, 0],  
+              [1, 0, 1],  
+              [0, 0, 0],  
+              [1, 1, 0],  
+              [0, 1, 1],  
+              [1, 0, 1],  
+              [0, 1, 1],  
+              [0, 1, 1],  
+              [0, 0, 0]]
+
+l_rate = 0.1
+n_epoch = 5
+resposta = perceptron(datasetTreinoAND, datasetAND, l_rate, n_epoch) 
+
